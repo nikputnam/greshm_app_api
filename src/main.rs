a494e2837a214e33f479cb06e17b69ab44ce1b24
Nik Putnam <nputnam@gmail.com>
2019-04-12 15:53:00 -0400

    working version of simple jwt creation and validation:
    token=`curl -X POST -H 'Accept: application/json' -H 'Content-Type: application/json' --data '{"username":"nik","password":"123pws","rememberMe":false}' http://localhost:8000/auth`
    curl -H 'Accept: application/json' -H "Authorization: ${token}" http://localhost:8000/nik/1

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,11 @@ use rocket::http::uri::Origin;
 
 use rocket_contrib::json::Json;
 
+use std::time::{SystemTime, UNIX_EPOCH};
+
 pub extern crate crypto;
 //use crypto::sha2::Sha256;
-pub extern crate jwt;
+extern crate jsonwebtoken as jwt;
 //pub extern crate rustc_serialize;
 
 //use self::crypto::digest::Digest;
@@ -21,11 +23,15 @@ use self::crypto::sha2::Sha256;
 
 //////////  JWT validation
 
-use self::jwt::{
-    Header,
-    Registered,
-    Token,
-};
+use jwt::{encode, decode, Header, Algorithm, Validation};
+
+/// Our claims struct, it needs to derive `Serialize` and/or `Deserialize`
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Claims {
+    sub: String,
+    name: String,
+    iat: u128
+}
 
 #[derive(Debug)]
 struct ApiKey(String);
@@ -41,9 +47,28 @@ fn is_valid(key: &str) -> bool {
     key == "Bearer valid_api_key"
 }
 
-pub fn read_token(key: &str, request: &Request) -> Result<String, String> {
-    let token = Token::<Header, Registered>::parse(key)
-        .map_err(|_| "Unable to parse key".to_string())?;
+pub fn read_token(key: &str, request: &Request) -> Result<jwt::TokenData<Claims>, jwt::errors::Error> {
+
+    // Changing one parameter
+    let mut validation = Validation {leeway: 60, validate_exp: false, validate_nbf: false, ..Default::default()};
+
+//    let token = Token::<Header, Registered>::parse(key);
+    println!("{:?}",key);
+
+    let token = decode::<Claims>(&key, "secret_key".as_ref(), &validation);
+        println!("{:?}",token);
+    token
+/*
+    match token {
+        Err(e) => Err(e),
+        Ok(claims) => Ok(claims.sub)
+    }
+    */
+//        .map_err(|_| "Token not valid".to_string())?;
+//    println!("{:?}",token);
+ //   Ok(token.claims.sub) 
+    //.ok_or("Claims not valid".to_string())
+/*
     if token.verify(b"secret_key", Sha256::new()) {
         println!("token: {:?}",token);
         let uri: &Origin = request.uri();
@@ -52,6 +77,7 @@ pub fn read_token(key: &str, request: &Request) -> Result<String, String> {
     } else {
         Err("Token not valid".to_string())
     }
+    */
 }
 
 #[derive(Debug)]
@@ -68,6 +94,8 @@ impl<'a, 'r> FromRequest<'a, 'r> for ApiKey {
 
     fn from_request(request: &'a Request<'r>) -> request::Outcome<Self, Self::Error> {
         let keys: Vec<_> = request.headers().get("Authorization").collect();
+        println!("nkeys:  {}",keys.len());
+        println!("key: {:?}",keys);
         //for k in &keys {
         //    println!("{}", k)
         //}
@@ -76,7 +104,8 @@ impl<'a, 'r> FromRequest<'a, 'r> for ApiKey {
             return Outcome::Forward(());
         }
         match read_token(keys[0], request) {
-            Ok(claim) => Outcome::Success(ApiKey(claim)),
+//            Ok(claim) => Outcome::Success(ApiKey(claim.sub)),
+            Ok(claim) => Outcome::Success(ApiKey("test".to_string())),
             Err(_) => Outcome::Forward(())
         }
     }
@@ -86,7 +115,10 @@ impl<'a, 'r> FromRequest<'a, 'r> for ApiKey {
 
 #[post("/",format = "application/json", data = "<user>")]
 fn auth(user: Json<Login>) -> String {
-    format!("Hello {:?}\n",user)
+    let now: u128 = SystemTime::now().duration_since(UNIX_EPOCH).expect("Time went backwards").as_millis();
+    let my_claims = Claims { sub: "12445".to_string(), name: "nik".to_string(),iat: now };
+    let token = encode(&Header::default(), &my_claims, "secret_key".as_ref()).unwrap();
+    format!("{}\n",token)
 }
 
 #[get("/<name>/<age>")]
